f54fcf2c8c18415b13ac5ebbd6762532d7d333c9
ElEHsiang <hsiang023167@gmail.com>
2014-05-23 02:29:17 +0800

    Modified map drawing method split color define to pixelWorldColor.py

--- pixelWorld.py
+++ pixelWorld.py
@@ -1,28 +1,72 @@
 import pygame
 from pygame.locals import *
+from pixelWorldColor import *
 
+blockSize = 3 
 
-redColor = pygame.Color(255, 0, 0)
-greenColor = pygame.Color(0, 255, 0)
-blueColor = pygame.Color(0, 0, 255)
-whiteColor = pygame.Color(255, 255, 255)
+windowWidth, windowHeight = 640, 480
 
-worldHeight = 480
-worldWidth = 640
+worldHeight = 480/blockSize
+worldWidth = 640/blockSize
 worldMap = [[0]*worldHeight for x in range(worldWidth)]
 
+windowSurfaceObj = ()
 
 def setMap():
+
+    mudDepth = 80
     for x in range(worldWidth):
-        for y in range(20):
+        for y in range(mudDepth):
             worldMap[x][worldHeight-y-1] = 1
+    for x in range(worldWidth):
+        for y in range(2):
+            worldMap[x][worldHeight-y-1 - mudDepth] = 2
+
+def dumpMap():
+
+    f = open('dumpMap.txt','w')
+
+    for x in range(worldWidth):
+        for y in range(worldHeight):
+            f.write(str(worldMap[x][y]) + " ")
+        f.write('\n')
+
+def drawBlock():
+
+    pixArr = pygame.PixelArray(windowSurfaceObj)
+
+    for y in range(windowHeight / blockSize):
+        for x in range(windowWidth / blockSize):
+
+            blockType = worldMap[x][y]
+
+            drawPosX = x * blockSize
+            drawPosY = y * blockSize
+
+            if blockType == 0:
+                pass
+            if blockType == 1:
+                pixArr[drawPosX][drawPosY] = mudColor
+                pixArr[drawPosX+1][drawPosY] = mudMixColor
+                pixArr[drawPosX][drawPosY+1] = mudMixColor
+                pixArr[drawPosX+1][drawPosY+1] = mudColor
+                pixArr[drawPosX+2][drawPosY] = mudColor
+                pixArr[drawPosX][drawPosY+2] = mudColor
+                pixArr[drawPosX+2][drawPosY+2] = mudColor
+                pixArr[drawPosX+1][drawPosY+2] = mudColor
+                pixArr[drawPosX+2][drawPosY+1] = mudColor
+            if blockType == 2:
+                pixArr[drawPosX][drawPosY] = grassColor
+                pixArr[drawPosX+1][drawPosY] = grassMixColor
+                pixArr[drawPosX][drawPosY+1] = grassMixColor
+                pixArr[drawPosX+1][drawPosY+1] = grassColor
+    del pixArr
 
 
 if __name__ == '__main__':
     pygame.init()
     fpsClock = pygame.time.Clock()
 
-    windowWidth, windowHeight = 640, 480
     windowSurfaceObj = pygame.display.set_mode((windowWidth, windowHeight))
 
     pygame.display.set_caption("Pixel World")
@@ -35,16 +79,7 @@ if __name__ == '__main__':
         windowSurfaceObj.fill(whiteColor)
         windowSurfaceObj.blit(player, (100, 100))
 
-        pixArr = pygame.PixelArray(windowSurfaceObj)
-
-        for y in range(worldHeight):
-            for x in range(worldWidth):
-                if worldMap[x][y] == 1:
-                    pixArr[x][y] = redColor
-                else:
-                    pixArr[x][y] = blueColor
-        del pixArr
-
+        drawBlock()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
---
+++ pixelWorldColor.py
@@ -0,0 +1,14 @@
+import pygame
+from pygame.locals import *
+
+redColor = pygame.Color(255, 0, 0)
+greenColor = pygame.Color(0, 255, 0)
+blueColor = pygame.Color(0, 0, 255)
+whiteColor = pygame.Color(255, 255, 255)
+
+mudColor = pygame.Color(114, 108, 63)
+mudMixColor = pygame.Color(143,116,63)
+
+grassColor = pygame.Color(89, 166, 62)
+grassMixColor = pygame.Color(64, 199, 109)
+
